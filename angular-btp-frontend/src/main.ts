2ffd6b324d6d5af15507391e4d18216421389be5
Abdelmoughit Mamdouh <abmamdouh@hps-worldwide.com>
2025-08-15 11:52:52 +0100

    frontend changes

--- angular-btp-frontend/src/main.ts
+++ angular-btp-frontend/src/main.ts
@@ -1,16 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi } from '@angular/common/http';
-import { JwtInterceptor } from './app/core/interceptors/jwt.interceptor';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()), // Ensure this is present
-  { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    provideHttpClient()
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
--- spring-boot-btp-backend/src/main/java/com/btp/dto/EmployeDTO.java
+++ spring-boot-btp-backend/src/main/java/com/btp/dto/EmployeDTO.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
